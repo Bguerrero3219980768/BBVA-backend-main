f927809957e9fad8189cafe507f05ec76ce731aa
brayanguerreo123 <112359242+Bguerrero3219980768@users.noreply.github.com>
2023-11-20 14:32:57 -0500

    .

--- src/test/java/com/ufps/demo/BbvaApplicationTests.java
+++ src/test/java/com/ufps/demo/BbvaApplicationTests.java
@@ -1,13 +1,71 @@
 package com.ufps.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-@SpringBootTest
-class BbvaApplicationTests {
+public class ConexionMySQL {
 
-	@Test
-	void contextLoads() {
+	private Connection con = null;
+	private static ConexionMySQL db;
+	private PreparedStatement preparedStatement;
+	
+	private static final String url = "jdbc:mysql://localhost:3306/";
+	private static final String dbName = "bancoparcial";
+	private static final String driver = "com.mysql.jdbc.Driver";
+	private static final String userName = "root";
+	private static final String password = "";
+	
+	
+	public ConexionMySQL () {
+	
+		try {
+			Class.forName(driver).newInstance();
+			con = (Connection)DriverManager.getConnection(url+dbName,userName,password);
+					
+		}catch (InstantiationException | IllegalAccessException 
+				| ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		}
+	
+	public void cerrarConexion() {
+		try {
+			con.close();
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
 	}
-
+	
+	public static ConexionMySQL getConexion() {
+		if(db == null){
+			db = new ConexionMySQL();
+		}
+		return db;
+	}
+	
+	public ResultSet query() throws SQLException {
+		ResultSet res = preparedStatement.executeQuery();
+		return res;
+	}
+	
+	public int execute() throws SQLException{
+		int result = preparedStatement.executeUpdate();
+		return result;
+	}
+	
+	public Connection getCon() {
+		return this.con;
+	}
+	
+	public PreparedStatement setPreparedStatement(String sql) throws SQLException{
+	
+		this.preparedStatement = con.prepareStatement(sql);
+		return this.preparedStatement;
+	} 
 }
